94f0c3f78c48a1800c2f16cf8696ef50ce83f3ef
lepela <lepelaka@gmail.com>
2025-04-09 10:17:37 +0900

    최초 커밋

---
+++ lesson01/src/lesson01/HelloWorld.java
@@ -0,0 +1,18 @@
+package lesson01;
+
+/**
+ * @author 박종민
+ * @since 1.0.1
+ */
+
+public class HelloWorld {
+	// main ctrl + space
+	public static void main(String[] args) {
+		// sout ctrl + space
+		System.out.println("Hello World"); // Ctrl + F11
+		// camel, snake
+		String name = "박종민";
+		System.out.println(name);
+		System.out.println(name.lastIndexOf("종"));
+	}
+}
